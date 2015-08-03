693c14a3ea9288eeb874de30db00c252204201cc
Connie Skomra <connie@cskomra.com>
2015-08-03 16:37:35 -0400

    removed "Training" from "Skills" (to test change/commit of new branch)

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,7 +11,6 @@ var bio = {
 	"welcomeMessage": "Welcome!",
 	"skills": ["Software Development",
 		"Project Management",
-		"Training",
 		"Strategic Planning",
 		"Analysis",
 		"Product Development",
