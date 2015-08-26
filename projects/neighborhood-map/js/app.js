0c1bb35fa8647f3a1bc2d08c6d0fce2fb72af8ae
Connie Skomra <connie@cskomra.com>
2015-08-26 14:50:28 -0400

    replace with new design

--- projects/neighborhood-map/js/app.js
+++ projects/neighborhood-map/js/app.js
@@ -1,111 +1,63 @@
-$(function() {
-	var map = {};
-	var data = {
-		mapMarkers: [],
-		places: [],
-		infowindow: {},
-		service: {}
-	};
+var data = {
+    mapMarkers: [{name: 'marker1'}, {name:'marker2'}, {name:'marker3'}],
+    placeTypes: ['gas_station', 'pet_store']
+};
 
-	var views = {
-		//===== VIEW PROPERTIES =====
-		map: document.getElementById('map'),
-		//===== VIEW OBJECTS =====
-		mapView: {
-			//init
-			initialize: function() {
-				var powell = {lat: 40.1583, lng: -83.0742};
-				map = new google.maps.Map(views.map, {
-				center: powell,
-				zoom: 17
-				});
+var mapView = {
+    gMap: new google.maps.Map(document.getElementById('map'), {
+        //center: self.locationData()[0].latLng,
+        center: {lat: 40.1583, lng: -83.0742},
+        zoom: 15
+        }),
+    infowindow: new google.maps.InfoWindow(),
 
-				data.infowindow = new google.maps.InfoWindow();
-				data.service = new google.maps.places.PlacesService(map);
-				//service.nearbySearch({
-				data.service.radarSearch({
-				location: powell,
-				radius: 3200,
-				types: ["gas_station", "restaurant", "store"],
-				//rankBy: google.maps.places.RankBy.DISTANCE
-				}, viewModel.callback);
-				//render
-			}
-		},
-		placesListView: {
-			//===== VIEW PROPERTIES =====
-			HTMLplacesListStart: '<h2 id="places-heading">Places</h2><ul id="placesList" class="flex-box"></ul>',
-			HTMLplace: '<li class="flex-item" data-bind="text data.places"><span class="white-text">%place-name%</span></li>',
-			//===== VIEW OBJECTS =====
-			initialize: function(){
-				$('#places').append(this.HTMLplacesListStart);
-				this.render();
-			},
-			render: function(){
-				viewModel.setPlaceDetails();
-			}
-		}
-	};
+    createMarker: function(place){
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+            map: this.gMap,
+            position: place.geometry.location,
+            place_id: place.place_id,
+            animation: google.maps.Animation.DROP
+        });
+        google.maps.event.addListener(marker, 'click', function() {
+            mapView.infowindow.setContent(place.name);
+            //TODO: setContent (other content for each place)
+            mapView.infowindow.open(mapView.gMap, this);
+            });
+    },
+    searchNearby: function(){
+        var loc = {lat: 40.1583, lng: -83.0742};
+        var service = new google.maps.places.PlacesService(mapView.gMap);
+        var requestObj = {
+            location: loc,
+            radius: 2000,
+            types: data.placeTypes
+        };
+        service.nearbySearch(requestObj,
+            function(results, status){
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    for (var i = 0; i < results.length; i++) {
+                        mapView.createMarker(results[i]);
+                        koViewModel.mapMarkers.push(results[i]);
+                        console.log(results[i]);
+                    }
+                }
+            }
+        );
+    },
+};
+var koViewModel = {
+    mapMarkers: ko.observableArray(data.mapMarkers),
+    placeTypes: ko.observableArray(data.placeTypes),
+    mapView: mapView.searchNearby(),
+    filterMarkers: function(){
+        console.log('filtering');
+        //set mapMarkers() based on types checked
+    },
+    getMarkerName: function(index){
+        var markers = koViewModel.getMapMarkers();
+        return markers.length;
+    }
+};
 
-	var viewModel = {
-		//data
-		mMarkers: ko.observableArray(data.mapMarkers),
-		//behavior
-		initialize: function(){
-			views.mapView.initialize();
-			views.placesListView.initialize();
-			ko.applyBindings(viewModel);
-		},
-		getMap: function(){
-			return map;
-		},
-		callback: function(results, status){
-			if (status === google.maps.places.PlacesServiceStatus.OK) {
-				console.log("results length = " + results.length);
-				for (var i = 0; i < results.length; i++) {
-					viewModel.createMarker(results[i]);
-					data.mapMarkers.push(results[i]);
-				}
-				views.placesListView.render();
-			}
-		},
-		setPlaceDetails: function(){
-			for (var i = 0; i < data.mapMarkers.length; i++) {
-				var place_id = data.mapMarkers[i].place_id;
-				var request = { placeId: place_id };
-				service = new google.maps.places.PlacesService(viewModel.getMap());
-				service.getDetails(request, callback);
-				function callback(place, status) {
-					if (status == google.maps.places.PlacesServiceStatus.OK) {
-						data.places.push(place);
-						var formattedPlace = views.placesListView.HTMLplace.replace("%place-name%", place.name);
-						$('#places').append(formattedPlace);
-					}
-				}
-			}
-		},
-		getMarkerDetails: function(place){
-			var request = {
-				placeId: place.id
-			};
-			return data.service.getDetails(request, status);
-		},
-		detailsCallback: function(placeDetails, status){
-			if(status == google.maps.places.PlacesServiceStatus.OK) {
-				console.log(placeDetails);
-			}
-		},
-		createMarker: function(place){
-			var placeLoc = place.geometry.location;
-			var marker = new google.maps.Marker({
-				map: viewModel.getMap(),
-				position: place.geometry.location
-			});
-			google.maps.event.addListener(marker, 'click', function() {
-				data.infowindow.setContent(place.name);
-				data.infowindow.open(viewModel.getMap(), this);
-			});
-		}
-	} // end viewModel
-viewModel.initialize();
-}); // end $(function(){})
+ko.applyBindings(koViewModel);
