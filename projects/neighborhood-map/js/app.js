4fb3bd694ea3138a9fe624c8dfa808efa4c3517b
Connie Skomra <connie@cskomra.com>
2015-09-02 18:06:49 -0400

    push data as markers, not places data-bind title

--- projects/neighborhood-map/js/app.js
+++ projects/neighborhood-map/js/app.js
@@ -7,13 +7,13 @@ var mapView = {
     gMap: new google.maps.Map(document.getElementById('map'), {
         //center: self.locationData()[0].latLng,
         center: {lat: 40.1583, lng: -83.0742},
-        zoom: 15
+        zoom: 13
         }),
     infowindow: new google.maps.InfoWindow(),
-
     createMarker: function(place){
         var placeLoc = place.geometry.location;
 
+        //return place types that matched selected filters
         var type = function(place){
             var types = place.types;  // marker-specific info
             var typesStr = '';
@@ -25,17 +25,18 @@ var mapView = {
             return typesStr;
         };
 
+        //create a marker for the place
         var marker = new google.maps.Marker({
             map: this.gMap,
-            position: place.geometry.location,
+            position: placeLoc,
             place_id: place.place_id,
             animation: google.maps.Animation.DROP,
             title: type(place) + " - " + place.name
         });
 
-
+        //add click listener to marker
         google.maps.event.addListener(marker, 'click', function() {
-            //TODO: add ajax content for each place
+            //TODO: clarify the wiki link for user
             var name = place.name;
             console.log(encodeURIComponent(name));
             var filterType = '<strong>' + name + '</strong>';
@@ -66,7 +67,8 @@ var mapView = {
                 }
             });
             mapView.infowindow.open(mapView.gMap, this);
-            });
+        });
+        return marker;
     },
     searchNearby: function(){
         var loc = {lat: 40.1583, lng: -83.0742};
@@ -80,27 +82,84 @@ var mapView = {
             function(results, status){
                 if (status === google.maps.places.PlacesServiceStatus.OK) {
                     for (var i = 0; i < results.length; i++) {
-                        mapView.createMarker(results[i]);
-                        koViewModel.mapMarkers.push(results[i]);
-                        console.log(results[i]);
-                    }
+                        koViewModel.mapMarkers.push(mapView.createMarker(results[i]));
+                        //koViewModel.mapMarkers.push(results[i]);
+                        //console.log(results[i]);
+                    };
+                    //make sure *markers* array created in mapMarkers()
+                    //these are markers in the koViewModel.mapMarkers() array:
+                    //console.log(koViewModel.mapMarkers());
                 }
             }
         )
+    },
+    initSearchPlaces: function() {
+        var input = document.getElementById('search-input');
+        var searchBox = new google.maps.places.SearchBox(input);
+
+        mapView.gMap.addListener('bounds_changed', function(){
+            searchBox.setBounds(mapView.gMap.getBounds());
+        });
+
+        searchBox.addListener('places_changed', function() {
+            var places = searchBox.getPlaces();
+
+            if (places.length == 0) {
+                return;
+            }
+
+            //clear out the old markers
+            //console.log("clearing...");
+            //console.log(koViewModel.mapMarkers());
+
+            console.log('entering for loop');
+            console.log(data.mapMarkers);
+            for( var i = 0; i < data.mapMarkers.length; i++) {
+                console.log(data.mapMarkers[i]);
+                data.mapMarkers[i].getPosition();
+            }
+
+
+            koViewModel.mapMarkers([]);
+            console.log("cleared?");
+            console.log(koViewModel.mapMarkers());
+
+            var bounds = new google.maps.LatLngBounds();
+            places.forEach(function(place) {
+                var icon = {
+                    url: place.icon,
+                    size: new google.maps.Size(71, 71),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(17, 34),
+                    scaledSize: new google.maps.Size(25, 25)
+                };
+
+                //Create a marker for each place.
+                koViewModel.mapMarkers().push(new google.maps.Marker( {
+                    map: mapView.gMap,
+                    //icon: icon,
+                    title: place.name,
+                    position: place.geometry.location
+                }));
+
+                if (place.geometry.viewport) {
+                    // Only geocodes have viewport.
+                    bounds.union(place.geometry.viewport);
+                } else {
+                    bounds.extend(place.geometry.location);
+                }
+            });
+            mapView.gMap.fitBounds(bounds);
+        })
     }
 };
 var koViewModel = {
     mapMarkers: ko.observableArray(data.mapMarkers),
     placeTypes: ko.observableArray(data.placeTypes),
-    //call this on click of submit button, pass in search field value latlng
-    mapView: mapView.searchNearby(),
+    searches: [ mapView.searchNearby(), mapView.initSearchPlaces()],
     filterMarkers: function(){
         console.log('filtering');
         //set mapMarkers() based on types checked
-    },
-    getMarkerName: function(index){
-        var markers = koViewModel.getMapMarkers();
-        return markers.length;
     }
 };
 
